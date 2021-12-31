d140a1d6a9e79e7a669bfafe6a65875612c71864
MaxwellJalves <maxwell880@gmail.com>
2021-12-31 17:34:14 -0300

    adicionado o botão + os possiveis types

--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,13 +1,29 @@
+import React from "react";
+import Button from "../Botao/Button";
 import "./style.scss";
 const Formulario = () => {
+  const debug = (evento: React.FormEvent<HTMLElement>) => {
+    evento.preventDefault();
+  };
   return (
-    <form className="container">
-      <label htmlFor="descricao">Descrição</label>
-      <input type="text" name="descricao" id="descricao" />
+    <>
+      <form className="container">
+        <label htmlFor="descricao">Descrição</label>
+        <input type="text" name="descricao" id="descricao" required />
 
-      <label htmlFor="descricao">Duração</label>
-      <input type="date" name="descricao" id="descricao" />
-    </form>
+        <label htmlFor="tempo">Duração</label>
+        <input type="date" name="tempo" id="tempo" required />
+        <>
+          <Button
+            tipo="submit"
+            titulo="Registrar"
+            onClick={(e) => {
+              debug(e);
+            }}
+          />
+        </>
+      </form>
+    </>
   );
 };
 export default Formulario;
