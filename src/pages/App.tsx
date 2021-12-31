6e9614c6f2a85e08db0d1564aac4c40ca222b3af
MaxwellJalves <maxwell880@gmail.com>
2021-12-31 16:49:45 -0300

    adicionado formulario e estilização

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Formulario from "../components/Formulario/Formulario";
 import "./style.scss";
 const App = () => {
   return (
     <div className="AppStyle">
-      <h2>Projeto</h2>
+      <Formulario />
     </div>
   );
 };
