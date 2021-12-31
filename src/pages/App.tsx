7dc79c87395d632433ba303b137e6a503a3d21f4
MaxwellJalves <maxwell880@gmail.com>
2021-12-31 15:59:15 -0300

    adicionando estilo base do projeto

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./style.scss";
 const App = () => {
   return (
-    <>
+    <div className="AppStyle">
       <h2>Projeto</h2>
-    </>
+    </div>
   );
 };
 
